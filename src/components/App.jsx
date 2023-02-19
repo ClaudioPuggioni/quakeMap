cbbd5f5d5272fabd4f5a390f29637653662337be
Claudio Puggioni <76516704+ClaudioPuggioni@users.noreply.github.com>
2023-02-19 19:40:39 +0700

    Fixed incorrect socket address

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import Map from "./Map";
 import { io } from "socket.io-client";
-const socket = io.connect("http://127.0.0.1:1111");
+// const socket = io.connect("http://127.0.0.1:1111")
+const socket = io.connect("https://quakebot.onrender.com/:10000");
 
 export default function App() {
   useEffect(() => {
