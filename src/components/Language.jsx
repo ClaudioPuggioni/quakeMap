945354d2f4a8324582571a5c2af2482346884459
Claudio Puggioni <76516704+ClaudioPuggioni@users.noreply.github.com>
2023-03-13 00:19:47 +0700

    fixed language

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useMemo } from "react";
-
 const POSITION_CLASSES = {
   bottomleft: "leaflet-bottom leaflet-left",
   bottomright: "leaflet-bottom leaflet-right",
@@ -8,33 +6,6 @@ const POSITION_CLASSES = {
 };
 
 export function Language({ position, language, handleSelect }) {
-  useEffect(() => {
-    console.log("language:", language);
-  }, [language]);
-
-  //   const minimap = useMemo(
-  //     () => (
-  //       <div className="flex flex-col justify-center items-center pb-1 gap-1">
-  //         <div className="text-[9px] text-black">{language === "tr" ? "Sabitleme Dili" : language === "en" ? "Pin Language" : "ERROR"}:</div>
-  //         <div className="flex px-[5px] w-[79px] justify-between gap-[5px]">
-  //           <img
-  //             className={`cursor-pointer ${language === "tr" ? "outline outline-orange-500 outline-offset-[1px]" : ""}`}
-  //             onClick={() => handleSelect("tr")}
-  //             src="/assets/icons/turkey.png"
-  //             alt="Turkey Flag"
-  //           />
-  //           <img
-  //             className={`cursor-pointer ${language === "en" ? "outline outline-orange-500 outline-offset-[1px]" : ""}`}
-  //             onClick={() => handleSelect("en")}
-  //             src="/assets/icons/united-kingdom.png"
-  //             alt="United Kingdom Flag"
-  //           />
-  //         </div>
-  //       </div>
-  //     ),
-  //     []
-  //   );
-
   const positionClass = (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright;
   return (
     <div className={positionClass}>
